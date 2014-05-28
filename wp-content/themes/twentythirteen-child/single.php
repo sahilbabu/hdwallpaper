e8404421da47575ed54b3307bd772e263c398b5a
sahilbabu <sahil_bwp@yahoo.com>
2014-05-28 16:07:37 +0500

    fb debugging

--- wp-content/themes/twentythirteen-child/single.php
+++ wp-content/themes/twentythirteen-child/single.php
@@ -221,7 +221,7 @@ get_header();
                 <div class="recentpostOuter">
                     <div id="fb-root"></div>
                     <script src="http://connect.facebook.net/en_US/all.js#appId=1459918874245839&amp;xfbml=1"></script>
-                    <fb:comments xid="<?php the_ID(); ?>" numposts="10" width="569" publish_feed="true"></fb:comments>
+                    <fb:comments xid="<?php the_ID(); ?>" numposts="10" width="569px" publish_feed="true"></fb:comments>
                 </div>
                 <!-- // fb comments  -->  
             </div>
