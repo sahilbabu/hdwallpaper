ff485920d6bcd139ed0d5d5d8f09164b004a2445
sahilbabu <sahil_bwp@yahoo.com>
2014-05-28 12:02:02 +0500

    css style

--- wp-content/themes/twentythirteen-child/single.php
+++ wp-content/themes/twentythirteen-child/single.php
@@ -57,7 +57,7 @@ get_header(); ?>
                                </div>
                                <div class="clr"></div>
                                <div class="extra-cont">
-                                    <div class="row-fluid">
+                                    <div class="row-fluid plainwidget">
                                         <div class="span3"><h4 class="widgettitle">HD</h4></div>
                                         <div class="span9">
                                             <a class="btn"  href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1600&h=1200">1600X1200</a>
@@ -74,7 +74,7 @@ get_header(); ?>
                                             <a class="btn"  href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1024&h=576">1024X576</a>
                                             <a class="btn"  href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=960&h=540">960X540</a></div>
                                     </div>
-                                    <div class="row-fluid">
+                                    <div class="row-fluid plainwidget">
                                         <div class="span3"><h4 class="widgettitle">WIDE</h4></div>
                                         <div class="span9">
                                             <a class="btn"  href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1600&h=1200">1600X1200</a>
