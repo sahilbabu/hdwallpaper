03c53e2d8ea4d7bd330ef63791cc15f5c9962fed
sahilbabu <sahil_bwp@yahoo.com>
2014-05-28 11:41:27 +0500

    css changes

--- wp-content/themes/twentythirteen-child/single.php
+++ wp-content/themes/twentythirteen-child/single.php
@@ -57,97 +57,42 @@ get_header(); ?>
                                </div>
                                <div class="clr"></div>
                                <div class="extra-cont">
-                              
-                                                             
-                               <?php
-								$args = array(
-									'orderby'       => 'name', 
-									'order'         => 'ASC',
-									'hide_empty'    => true, 
-									'exclude'       => array(), 
-									'exclude_tree'  => array(), 
-									'include'       => array(),
-									'number'        => '', 
-									'fields'        => 'all', 
-									'slug'          => '', 
-									'parent'         => 0,
-									'hierarchical'  => true, 
-									'child_of'      => 0, 
-									'get'           => '', 
-									'name__like'    => '',
-									'pad_counts'    => false, 
-									'offset'        => '', 
-									'search'        => '', 
-									'cache_domain'  => 'core'
-								); 
-								$term_list = wp_get_post_terms($post->ID, 'resolution', $args);
-								
-								
-								   $cat_ids=array();
-															   
-								   $k=0;
-								   foreach($term_list as $cat)
-								   {
-									  
-									$cat_ids[$k]=$cat->term_id;  
-									$k++;
-								   }
-								
-								
-								
-									$terms_p = get_terms("resolution",$args);
-									
-									 if ( !empty( $terms_p ) && !is_wp_error( $terms_p ) ){
-										 
-										 foreach($terms_p as $term_p)
-										 {
-											if (in_array($term_p->term_id, $cat_ids)) 
-											{
-											$term_p = sanitize_term( $term_p, 'resolution' );
-								
-											$term_p_link = get_term_link( $term_p, 'resolution' );
-											$term_id = $term_p->term_id;
-											 ?>
-												<P><?php echo $term_p->name?></P>
-												<?php
-											 
-											$term_id = $term_p->term_id;
-											$taxonomy_name = 'resolution';
-											$termchildren = get_term_children( $term_id, $taxonomy_name );
-											if(!empty($termchildren))
-											{
-												
-												
-												?>
-												<div class="extra-produtct right">
-															   <P>
-												<?php
-											
-												foreach ( $termchildren as $child ) {
-													$term = get_term_by( 'id', $child, $taxonomy_name );
-													$term = sanitize_term( $term, 'resolution' );
-													if (in_array($term->term_id, $cat_ids)) 
-													{
-													$term_link = get_term_link( $term, 'resolution' );
-													
-													$dimention = explode("X",$term->name);
-													?>
-														<a href="<?php echo site_url('/');?>download.php?download_file=<?php print urlencode($large_image_full[0]);?>&w=<?php echo $dimention[0];?>&h=<?php echo $dimention[1];?>"><?php echo $term->name; ?></a>
-													
-													<?php
-													}
-													
-												}
-												?>
-												</p></div>
-												<?php
-											}
-											}
-										 }
-									 } 
-								
-								?>
-								</div> 
+                                    <div class="row-fluid">
+                                        <div class="span3">HD</div>
+                                        <div class="span9">
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1600&h=1200">1600X1200</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=3840&h=2160">3840X2160</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=3554&h=1999">3554X1999</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2880&h=1620">2880X1620</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2560&h=1440">2560X1440</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2400&h=1350">2400X1350</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2048&h=1152">2048X1152</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1920&h=1080">1920X1080</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1600&h=900">1600X900</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1366&h=768">1366X768</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1280&h=720">1280X720</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1024&h=576">1024X576</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=960&h=540">960X540</a></div>
+                                    </div>
+                                    <div class="row-fluid">
+                                        <div class="span3">Wide</div>
+                                        <div class="span9">
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1600&h=1200">1600X1200</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=3840&h=2160">3840X2160</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=3554&h=1999">3554X1999</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2880&h=1620">2880X1620</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2560&h=1440">2560X1440</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2400&h=1350">2400X1350</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=2048&h=1152">2048X1152</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1920&h=1080">1920X1080</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1600&h=900">1600X900</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1366&h=768">1366X768</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1280&h=720">1280X720</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=1024&h=576">1024X576</a>
+                                            <a href="http://www.wallrgb.com/download.php?download_file=http%3A%2F%2Fwww.wallrgb.com%2Fwp-content%2Fuploads%2F2014%2F05%2Fsky_sun_sea_path_reflection_clouds_ripples_horizon_line_wallpaper_3840x2160.jpg&w=960&h=540">960X540</a></div>
+                                    </div>
+
+                                </div>
                                <div class="product-main-baner content-area">
                            	   
                                 
